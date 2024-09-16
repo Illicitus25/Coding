90b9b51f91c72a9e0645e21b90c5c353b377a84e
Prakhyat Singh <professionalgamersss@gmail.com>
2024-09-16 14:48:00 +0530

    yes

---
+++ JAVA/CSE 330/reverseLL.java
@@ -0,0 +1,26 @@
+
+import java.util.LinkedList;
+
+public class reverseLL {
+    public static void main(String args[]){
+        LinkedList<Integer> list= new LinkedList<>();
+        LinkedList<Integer> list1= new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+"->");
+        }
+        while(!list.isEmpty()){
+        list1.add(list.getLast());
+        list.removeLast();
+        }
+        System.out.println();
+        for(int i=0;i<list1.size();i++){
+            System.out.print(list1.get(i)+"->");
+        }
+
+    }
+}
